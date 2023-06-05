ac78c3cd22445b3c534f09bfd7746e34f2f829a4
Andrei Manea <_AManea.AZ@sodexo.com>
2023-06-05 22:35:54 +0300

    New bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import MyComponent from "./MyComponent";
+import UserList from "./UserList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <MyComponent name={"andrew"} />
+        <UserList />
       </header>
     </div>
   );
---
+++ src/UserList.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const mockUsers: User[] = [
+  {
+    id: 1,
+    name: "John",
+  },
+  {
+    id: 2,
+    name: "Doe",
+  },
+];
+
+const UserList: React.FC = () => {
+  const [users, setUsers] = useState<string[]>([]); // Error Here
+
+  useEffect(() => {
+    // Simulate an API call with a delay
+    setTimeout(() => {
+      setUsers(mockUsers); // Error Here
+    }, 2000);
+  }, []);
+
+  return (
+    <ul>
+      {users.map(
+        (
+          user // Error Here
+        ) => (
+          <li key={user.id}>{user.name}</li>
+        )
+      )}
+    </ul>
+  );
+};
+
+export default UserList;
