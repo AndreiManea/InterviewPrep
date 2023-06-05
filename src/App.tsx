6eaa913b9b71a5f127980ef747308667bf0a9cfe
Adina Totorean <adina.totorean@hcl.com>
2023-06-05 22:20:28 +0300

    fixed ts bug

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <MyComponent name={5} />
+        <MyComponent name={"andrew"} />
       </header>
     </div>
   );
